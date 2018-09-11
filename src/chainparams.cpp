984cb34ba3aa2f7c40e38fce44421d657c5c541f
top1st <riguang102@gmail.com>
2018-09-11 16:35:41 +0800

    update pchMessgeStart

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,10 +122,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x90;
-        pchMessageStart[1] = 0xc4;
-        pchMessageStart[2] = 0xfd;
-        pchMessageStart[3] = 0xe9;
+        pchMessageStart[0] = 0xa4;
+        pchMessageStart[1] = 0xa1;
+        pchMessageStart[2] = 0xb0;
+        pchMessageStart[3] = 0xb3;
         vAlertPubKey = ParseHex("0000098d3ba6ba6e7423fa5cbd6a89e0a9a5348f88d332b44a5cb1a8b7ed2c1eaa335fc8dc4f012cb8241cc0bdafd6ca70c5f5448916e4e6f511bcd746ed57dc50");
         nDefaultPort = 52572;
         bnProofOfWorkLimit = ~uint256(0) >> 20; // DAPScoin starting difficulty is 1 / 2^12
@@ -251,10 +251,10 @@ public:
     {
         networkID = CBaseChainParams::TESTNET;
         strNetworkID = "test";
-        pchMessageStart[0] = 0x45;
-        pchMessageStart[1] = 0x76;
-        pchMessageStart[2] = 0x65;
-        pchMessageStart[3] = 0xba;
+        pchMessageStart[0] = 0xa5;
+        pchMessageStart[1] = 0xa2;
+        pchMessageStart[2] = 0xb1;
+        pchMessageStart[3] = 0xb4;
         vAlertPubKey = ParseHex("000010e83b2703ccf322f7dbd62dd5855ac7c10bd055814ce121ba32607d573b8810c02c0582aed05b4deb9c4b77b26d92428c61256cd42774babea0a073b2ed0c9");
         nDefaultPort = 52574;
         nEnforceBlockUpgradeMajority = 51;
