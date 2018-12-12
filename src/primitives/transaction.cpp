f5b8c9fe6a1bcf17eb80d1a81c9f485cef827d8c
Cam <damvananh91@gmail.com>
2018-12-12 19:05:36 +0700

    DAPS-321 #ready-for-qa #time 1.5h #comment Fix public key GetHex function, update transaction and CMutableTransaction, and re-mine genesis blocks for all mainnet, testnet, regtest and update chain params

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -90,8 +90,8 @@ std::string CTxOut::ToString() const
     return strprintf("CTxOut(nValue=%d.%08d, scriptPubKey=%s)", nValue / COIN, nValue % COIN, scriptPubKey.ToString().substr(0,30));
 }
 
-CMutableTransaction::CMutableTransaction() : nVersion(CTransaction::CURRENT_VERSION), nLockTime(0) {}
-CMutableTransaction::CMutableTransaction(const CTransaction& tx) : nVersion(tx.nVersion), vin(tx.vin), vout(tx.vout), nLockTime(tx.nLockTime) {}
+CMutableTransaction::CMutableTransaction() : nVersion(CTransaction::CURRENT_VERSION), nLockTime(0), txPubKey(), hasPaymentID(0), paymentID(0) {}
+CMutableTransaction::CMutableTransaction(const CTransaction& tx) : nVersion(tx.nVersion), vin(tx.vin), vout(tx.vout), nLockTime(tx.nLockTime), txPubKey(tx.txPubKey), hasPaymentID(tx.hasPaymentID), paymentID(tx.paymentID) {}
 
 uint256 CMutableTransaction::GetHash() const
 {
@@ -118,9 +118,9 @@ void CTransaction::UpdateHash() const
     *const_cast<uint256*>(&hash) = SerializeHash(*this);
 }
 
-CTransaction::CTransaction() : hash(), nVersion(CTransaction::CURRENT_VERSION), vin(), vout(), nLockTime(0) { }
+CTransaction::CTransaction() : hash(), nVersion(CTransaction::CURRENT_VERSION), vin(), vout(), nLockTime(0), txPubKey(), hasPaymentID(0), paymentID(0) { }
 
-CTransaction::CTransaction(const CMutableTransaction &tx) : nVersion(tx.nVersion), vin(tx.vin), vout(tx.vout), nLockTime(tx.nLockTime) {
+CTransaction::CTransaction(const CMutableTransaction &tx) : nVersion(tx.nVersion), vin(tx.vin), vout(tx.vout), nLockTime(tx.nLockTime), txPubKey(tx.txPubKey), hasPaymentID(tx.hasPaymentID), paymentID(tx.paymentID) {
     UpdateHash();
 }
 
@@ -130,6 +130,9 @@ CTransaction& CTransaction::operator=(const CTransaction &tx) {
     *const_cast<std::vector<CTxOut>*>(&vout) = tx.vout;
     *const_cast<unsigned int*>(&nLockTime) = tx.nLockTime;
     *const_cast<uint256*>(&hash) = tx.hash;
+    *const_cast<CPubKey*>(&txPubKey) = tx.txPubKey;
+    *const_cast<char*>(&hasPaymentID) = tx.hasPaymentID;
+    *const_cast<uint64_t*>(&paymentID) = tx.paymentID;
     return *this;
 }
 
