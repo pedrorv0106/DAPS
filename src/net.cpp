8bb48b4a5f23520687f4c4ad1e0e4f47edd31314
Cam <damvananh91@gmail.com>
2019-01-22 13:12:19 +0700

    fix compilation error

--- src/net.cpp
+++ src/net.cpp
@@ -315,7 +315,7 @@ bool IsLocal(const CService &addr) {
 /** check whether a given network is one we can probably connect to */
 bool IsReachable(enum Network net) {
     LOCK(cs_mapLocalHost);
-    return vfReachable[net] && !vfLimited[net];
+    return !vfLimited[net];
 }
 
 /** check whether a given address is in a network we can probably connect to */
