ea82c6493561ce31259039d0addeacf03d4d8e2c
Cam <damvananh91@gmail.com>
2019-01-27 07:43:06 +0700

    fix coin supply

--- src/miner.cpp
+++ src/miner.cpp
@@ -472,6 +472,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         pblock->vtx[0].vin[0].scriptSig = CScript() << nHeight << OP_0;
         LogPrintf("%: Coinbase value without fee, value = %d, fee = %d", __func__, pblock->vtx[0].vout[0].nValue, nFees);
         if (!fProofOfStake) {
+            pblock->vtx[0] = txNew;
             pblock->vtx[0].vout[0].nValue += nFees * COIN;
             pblocktemplate->vTxFees[0] = nFees * COIN;
         }
