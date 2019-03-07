eb96ece0d0a19f8bbba31044520ffb18f09cd7b9
Cam <damvananh91@gmail.com>
2019-03-08 05:25:50 +0700

    fix key spent check in wallet

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1422,6 +1422,8 @@ CAmount CWallet::GetBalance()
                         if (!IsSpent(pcoin->GetHash(), i) || !IsKeyImageSpend1(keyImageHex, chainActive.Tip()->nHeight)) {
                             keyImagesSpends[keyImageHex] = false;
                             nTotal += getCTxOutValue(*pcoin, pcoin->vout[i]);
+                        } else {
+                            keyImagesSpends[keyImageHex] = true;
                         }
                     }
                 }
