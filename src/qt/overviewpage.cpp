109db16b83f75200d64b3c7fda35cc7b866172db
root <ryan.maciomaire@gmail.com>
2019-03-11 11:05:37 -0700

    DAPS-490 add window title bar

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -132,7 +132,7 @@ OverviewPage::OverviewPage(QWidget* parent) : QDialog(parent),
     pingNetworkInterval = new QTimer();
 
     initSyncCircle(.8);
-    updateRecentTransactions();
+    // #FLAG updateRecentTransactions();
 }
 
 void OverviewPage::handleTransactionClicked(const QModelIndex& index)
