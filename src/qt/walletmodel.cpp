e9d0ac781b2011740c106506decae75a912f57ab
Cam <damvananh91@gmail.com>
2019-03-20 12:08:54 +0700

    fix daps-521

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -782,7 +782,7 @@ std::map<QString, QString> getTx(CWallet* wallet, CWalletTx tx)
         if (wallet->RevealTxOutAmount(tx,out,vamount))
             totalamount+=vamount;   //this is the total output
     }
-
+    std::cout << "totalamount = " << totalamount << ", totalin"
     QList<TransactionRecord> decomposedTx = TransactionRecord::decomposeTransaction(wallet, tx);
     QList<QString> addressBook = getAddressBookData(wallet);
     std::map<QString, QString> txData;
@@ -807,6 +807,13 @@ std::map<QString, QString> getTx(CWallet* wallet, CWalletTx tx)
         case TransactionRecord::SendToAddress:
         case TransactionRecord::SendToOther:
             txData["type"] = QString("Sent");
+            totalamount = 0;
+            wallet->IsTransactionForMe(tx);
+            for (CTxOut out: tx.vout){
+                CAmount vamount;
+                if (wallet->RevealTxOutAmount(tx,out,vamount))
+                    totalamount+=vamount;   //this is the total output
+            }
             txData["amount"] = BitcoinUnits::format(0, totalIn - totalamount); //absolute value of total amount
             return txData;
             break;
