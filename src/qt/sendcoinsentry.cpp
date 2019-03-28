229cb48f551c26bebf929fa7924898a8e1c3ff24
damvananh91 <40625007+damvananh91@users.noreply.github.com>
2019-03-28 10:04:05 +0700

    Update sendcoinsentry.cpp

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -164,7 +164,7 @@ static inline int64_t roundint64(double d)
 
 CAmount SendCoinsEntry::getValidatedAmount() {
     double dAmount = ui->payAmount->text().toDouble();
-    if (dAmount <= 0.0 || dAmount > 2100000000.0)
+    if (dAmount < 0.0 || dAmount > 2100000000.0)
         throw runtime_error("Invalid amount, amount should be < 2.1B DAPS");
     CAmount nAmount = roundint64(dAmount * COIN);
     return nAmount;
