65b3001a27a7a65480a4d03899029f486b93fff2
Cam <damvananh91@gmail.com>
2019-04-04 08:36:34 +0700

    fix start alias button do not start create broad cast

--- src/qt/masternodelist.cpp
+++ src/qt/masternodelist.cpp
@@ -91,7 +91,7 @@ void MasternodeList::StartAlias(std::string strAlias)
             std::string strError;
             CMasternodeBroadcast mnb;
 
-            bool fSuccess = CMasternodeBroadcast::Create(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError, mnb);
+            bool fSuccess = activeMasternode.CreateBroadcast(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError, mnb);
 
             if (fSuccess) {
                 strStatusHtml += "<br>Successfully started masternode.";
