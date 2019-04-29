bd4c55879b3e15f4928d2be90b231aa2eb4e0074
zeus16 <dream0329@outlook.com>
2019-04-29 18:06:27 -0400

    DAPS-615 pos poa page add

--- assets/react/src/assets/views/components/footer/index.js
+++ assets/react/src/assets/views/components/footer/index.js
@@ -8,18 +8,18 @@ const Footer = (props) => {
     const Links = {
         headers: ["Company", "Learn", "Contact Us"],
         '0': [
-            <a href='https://dapscoin.com/roadmap/' target="_blank">About</a>,
-            <a href='https://dapscoin.com/team/' target="_blank">Team</a>,
+            <a key="About" href='https://dapscoin.com/roadmap/' target="_blank">About</a>,
+            <a key="Team" href='https://dapscoin.com/team/' target="_blank">Team</a>,
             // <a href='google.com'>Careers</a>,
         ],
         "1": [
-            <a href='https://dapscoin.com/roadmap/' target="_blank">Getting Started</a>,
-            <a href='https://dapscoin.com/daps-project-blog/' target="_blank">DAPS Blog</a>,
-            <a href='https://dapscoin.com/info/' target="_blank">What is DAPS?</a>,
+            <a key="Getting Started" href='https://dapscoin.com/roadmap/' target="_blank">Getting Started</a>,
+            <a key="DAPS Blog" href='https://dapscoin.com/daps-project-blog/' target="_blank">DAPS Blog</a>,
+            <a key="What is DAPS" href='https://dapscoin.com/info/' target="_blank">What is DAPS?</a>,
         ],
         "2": [
             // <a href='google.com'>Press</a>,
-            <a href='https://dapscoin.com/contact/' target="_blank">Support</a>,
+            <a key="Support" href='https://dapscoin.com/contact/' target="_blank">Support</a>,
             // <a href='google.com'>Status</a>,
         ],
         // "3": [<a href='google.com'>Local</a>],
@@ -27,12 +27,12 @@ const Footer = (props) => {
     const SocialLinks = {
         "headers": [],
         "0": [
-            <a href='https://discord.gg/w898czA' id='discordlink' className='SocialLink' target="_blank" ><div id='discord' /></a>,
-            <a href='https://www.facebook.com/dapscoinofficial/' id='facebooklink' className='SocialLink' target="_blank" ><div id='facebook' /></a>,
-            <a href='https://www.reddit.com/r/DAPSCoin/' id='redditlink' className='SocialLink' target="_blank"><div id='reddit' /></a>,
-            <a href='https://t.me/dapscoin' id='telegramlink' className='SocialLink' target="_blank" ><div id='telegram' /></a>,
-            <a href='https://www.linkedin.com/company/daps-coin/' id='linkedinlink' className='SocialLink' target="_blank"><div id='linkedin' /></a>,
-            <a href='https://twitter.com/DAPScoin' id='twitterlink' className='SocialLink' target="_blank" ><div id='twitter' /></a>,
+            <a key="discord" href='https://discord.gg/w898czA' id='discordlink' className='SocialLink' target="_blank" ><div id='discord' /></a>,
+            <a key="facebook" href='https://www.facebook.com/dapscoinofficial/' id='facebooklink' className='SocialLink' target="_blank" ><div id='facebook' /></a>,
+            <a key="reddit" href='https://www.reddit.com/r/DAPSCoin/' id='redditlink' className='SocialLink' target="_blank"><div id='reddit' /></a>,
+            <a key="telegram" href='https://t.me/dapscoin' id='telegramlink' className='SocialLink' target="_blank" ><div id='telegram' /></a>,
+            <a key="linkedin" href='https://www.linkedin.com/company/daps-coin/' id='linkedinlink' className='SocialLink' target="_blank"><div id='linkedin' /></a>,
+            <a key="twiiter" href='https://twitter.com/DAPScoin' id='twitterlink' className='SocialLink' target="_blank" ><div id='twitter' /></a>,
         ]
     }
 
