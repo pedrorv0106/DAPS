0abc86562f18da31aa3b25432a0a3e7f984dfc3c
zeus16 <dream0329@outlook.com>
2019-06-04 04:33:57 -0400

    DAPS-682 implement 2fa function

--- src/qt/2faconfirmdialog.cpp
+++ src/qt/2faconfirmdialog.cpp
@@ -1,12 +1,12 @@
-#include "2fadialog.h"
-#include "ui_2fadialog.h"
+#include "2faconfirmdialog.h"
+#include "ui_2faconfirmdialog.h"
 #include "receiverequestdialog.h"
-#include "include/qgoogleauth/qgoogleauth.h"
+
 #include <QDateTime>
 
-TwoFADialog::TwoFADialog(QWidget *parent) :
+TwoFAConfirmDialog::TwoFAConfirmDialog(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::TwoFADialog)
+    ui(new Ui::TwoFAConfirmDialog)
 {
     ui->setupUi(this);
 
@@ -45,12 +45,12 @@ TwoFADialog::TwoFADialog(QWidget *parent) :
 
 }
 
-TwoFADialog::~TwoFADialog()
+TwoFAConfirmDialog::~TwoFAConfirmDialog()
 {
     delete ui;
 }
 
-void TwoFADialog::on_acceptCode()
+void TwoFAConfirmDialog::on_acceptCode()
 {
     QString code;
     char code1, code2, code3, code4, code5, code6;
@@ -108,25 +108,12 @@ void TwoFADialog::on_acceptCode()
     code.sprintf("%c%c%c%c%c%c", code1, code2, code3, code4, code5, code6);
 
     QString codeSetting = settings.value("2FACode").toString();
-    if (codeSetting == "") {
-        QString result = "";
-        CWalletDB walletdb(pwalletMain->strWalletFile);
-        CAccount account;
-        walletdb.ReadAccount("", account);
-        CBitcoinAddress address(account.vchPubKey.GetID());
-        std::string addr = "";
-        for (char c : address.ToString()) {
-            if (!std::isdigit(c)) addr += c;
-        }
-                    
-        QString data;
-        data.sprintf("%s", addr.c_str());
-        result = QGoogleAuth::generatePin(data.toUtf8());
-        
-        if (result != code)
+    if (codeSetting != "") {
+        if (code != codeSetting)
             return;
 
-        settings.setValue("2FACode", code);
+        QDateTime current = QDateTime::currentDateTime();
+        settings.setValue("2FALastTime", current.toTime_t());
     }
 
     accept();
