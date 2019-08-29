2c62cb951946cf26de8c0dbc34daa8d208b94e68
lyricidal <lyricidal@gmail.com>
2019-08-28 22:33:18 -0400

    [Refactor] Move ThreadStakeMinter out of net.cpp to miner.cpp
    This moves the implementation function and thread creation to a more appropriate file (`miner.cpp`).
    
    https://github.com/PIVX-Project/PIVX/commit/42fe4040acff08450168416ccf35413abdc1747a

--- src/miner.cpp
+++ src/miner.cpp
@@ -935,4 +935,20 @@ void GenerateDapscoins(bool fGenerate, CWallet* pwallet, int nThreads)
         minerThreads->create_thread(boost::bind(&ThreadBitcoinMiner, pwallet));
 }
 
+// ppcoin: stake minter thread
+void static ThreadStakeMinter() {
+    boost::this_thread::interruption_point();
+    LogPrintf("ThreadStakeMinter started\n");
+    CWallet *pwallet = pwalletMain;
+    try {
+        BitcoinMiner(pwallet, true);
+        boost::this_thread::interruption_point();
+    } catch (std::exception &e) {
+        LogPrintf("ThreadStakeMinter() exception \n");
+    } catch (...) {
+        LogPrintf("ThreadStakeMinter() error \n");
+    }
+    LogPrintf("ThreadStakeMinter exiting,\n");
+}
+
 #endif // ENABLE_WALLET
