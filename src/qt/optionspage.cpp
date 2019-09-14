df5a2b8f44a48c36800c7629ec9bf77ba4861af6
lyricidal <lyricidal@gmail.com>
2019-09-14 01:09:27 -0400

    Remove Copy button for security

--- src/qt/optionspage.cpp
+++ src/qt/optionspage.cpp
@@ -550,20 +550,12 @@ void OptionsPage::onShowMnemonic() {
     if (!hdChainCurrent.GetMnemonic(mnemonic, mnemonicPass))
         return;
 
-
     QString mPhrase = std::string(mnemonic.begin(), mnemonic.end()).c_str();
     QMessageBox msgBox;
-    QPushButton *copyButton = msgBox.addButton(tr("Copy"), QMessageBox::ActionRole);
-    copyButton->setStyleSheet("background:transparent;");
-    copyButton->setIcon(QIcon(":/icons/editcopy"));
     msgBox.setWindowTitle("Mnemonic Recovery Phrase");
     msgBox.setText("Below is your Mnemonic Recovery Phrase, consisting of 24 seed words. Please copy/write these words down in order. We strongly recommend keeping multiple copies in different locations.");
     msgBox.setInformativeText("\n<b>" + mPhrase + "</b>");
     msgBox.setStyleSheet(GUIUtil::loadStyleSheet());
     msgBox.exec();
 
-    if (msgBox.clickedButton() == copyButton) {
-	//Copy Mnemonic Recovery Phrase to clipboard
-	GUIUtil::setClipboard(std::string(mnemonic.begin(), mnemonic.end()).c_str());
-    }
 }
