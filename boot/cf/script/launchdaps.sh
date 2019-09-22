f09ab952cb16bd4fb7d340f8618ea71b98aef612
Joseph E. Savard <josephsavard@gmail.com>
2019-09-22 12:16:35 -0500

    Added reysnc and refactor code

--- boot/cf/script/launchdaps.sh
+++ boot/cf/script/launchdaps.sh
@@ -3,14 +3,21 @@
 PID=0
 
 checkstatus () {
+     echo "Checking dapascoind status"
+     messagesleep 1
      PID=$(pgrep dapscoind)
-     if pgrep -q dapscoind 
+     if [ -z $PID ]
      then 
-         return $PID
+         return -1
      else 
          return $PID
      fi 
+     echo "Status Return Code:='"$PID"'"
 } 
+
+getpid() {
+  echo $(pgrep dapscoind) 
+}
 
 messagesleep () {
   x=0;   
@@ -25,8 +32,77 @@ messagesleep () {
   echo ""
 }
 
+start () {
+     echo "Attempting dapscoind Start"
+     PID=$(pgrep dapscoind)
+     if ! [ -z $PID ]
+     then 
+       echo "dapscoind already started on PID='"$PID"'" 
+       return $PID
+     else 
+       dapscoind --daemon 
+       messagesleep 7
+       checkstatus
+       echo "dapscoind started on PID='"$PID"'"
+       PID=$(pgrep dapscoind)
+       return $PID
+     fi 
+} 
+
+resync () {
+     echo "Attempting dapscoind resync"
+     PID=$(pgrep dapscoind)
+     if ! [ -z $PID ]
+     then 
+       echo "dapscoind already started on PID='"$PID"'" 
+       return $PID
+     else 
+       dapscoind -resync 
+       messagesleep 7
+       checkstatus
+       echo "dapscoind sresync issued and successful tarted on PID='"$PID"'"
+       PID=$(pgrep dapscoind)
+       return $PID
+     fi 
+} 
+
+stop () {
+    echo "Attempting dapscoind Stop"
+    PID=$(pgrep dapscoind)
+     if ! [ -z $PID ]
+          then     
+            echo "Stop dapscoind PID='"$PID"'" 
+            echo "Stop Execution : gracefully"
+            dapscoin-cli stop
+            messagesleep 7
+            checkstatus
+            echo "dapscoind successfully stopped @PID='"$PID"'" 
+          else
+            echo "dapscoind is not executiing: "     
+    fi
+}
+
+restart () {
+    echo "Attempting dapscoind Restart"
+    PID=$(pgrep dapscoind)
+    if ! [ -z $PID ]
+    then  
+          stop 
+          checkstatus
+          if  [ -z $PID ]
+          then     
+             echo "dapscoind ending... :"      
+          fi 
+     fi 
+     start
+     checkstatus
+     PID=$(pgrep dapscoind)
+     return $PID
+} 
+
 case $1 in
      status) 
+          echo "Retrieving Status" 
           checkstatus
           if ! [ -z $PID ]
           then     
@@ -36,31 +112,26 @@ case $1 in
           fi  
           ;;     
      restart) 
+          echo "Restarting dapscoind"
           checkstatus
-          if [ -z $PID ]
+          if ! [ -z $PID ]
           then     
-            echo echo " dapscoind --daemon"
+            echo echo "dapscoind not running"
+            messagesleep 7
+            start
+            checkstatus
             dapscoind --daemon
           else
             echo "dapscoind is executing on process ID: " $PID
-            echo "Kill PID:"$PID 
-            dapscoin-cli stop
-            messagesleep 7
-            echo "Restarting dapscoind"
-            dapscoind --daemon 
-            messagesleep 7
+            echo "Stopping dapscoind @ PID:"$PID " Before "
+            stop
             checkstatus
-            if ! [ -z $PID ]
-            then     
-               echo "Normal execution and responsive PID='"$PID"'"
-            else
-               echo "dapscoind not  responsive ending... :" 
-               exit -1     
-            fi 
+            messagesleep 7
+
           fi  
           ;;
      start)
-          echo "Going start"
+          echo "launch dapscoind: start"
           checkstatus
           if [ -z $PID ]
           then     
@@ -70,26 +141,20 @@ case $1 in
             messagesleep 7
           else
             echo "dapscoind is executing on process ID: " $PID
-            echo "Start aborting - please try inspect.sh restart"     
+            echo "Start aborting - please try launchdaps.sh restart"     
           fi  
           ;; 
      stop)
-          echo "Going Stop"
+          echo "Shutting down dapscoind: Stop"
           checkstatus          
-          if ! [ -z $PID ]
-          then     
-            echo "Stop dapscoind PID:" $PID 
-            echo "Stop Execution : gracefully"
-            dapscoin-cli stop
-            messagesleep 7
-          else
-            echo "dapscoind is executing on process ID: " $PID
-            echo "Aborting Start - please try inspect.sh restart"     
-          fi   
-          ;;     
+          stop
+          ;;
+     getpid)
+          getpid       
+          ;;        
      *)
-          echo "Invalid input - '"$1"'" m,
-          echo "Valid switches : start, stop, restart, status "
+          echo "Invalid input - '"$1"'"
+          echo "Valid switches : start, stop, restart, status, resync"
           ;;
 esac
  
