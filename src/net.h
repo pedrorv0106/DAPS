e2039b16d4d180129136edf4a64806febef6f850
zeus <dream0329@outlook.com>
2019-10-09 06:53:09 +0800

    DAPS-1038 fix typo in listbanned RPC message (#715)

--- src/net.h
+++ src/net.h
@@ -263,7 +263,7 @@ public:
     {
         switch (banReason) {
             case BanReasonNodeMisbehaving:
-                return "node misbehabing";
+                return "node misbehaving";
             case BanReasonManuallyAdded:
                 return "manually added";
             default:
