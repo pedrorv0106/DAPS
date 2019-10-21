b8829f3b841aa91e8915e1ef7acbf1db4365bc3c
JD <lyricidal@gmail.com>
2019-10-21 13:42:28 -0400

    DAPS-1195 Deny connections from peers who are running anything less than v1.0.3 (#773)
    * Disconnect from any peers running DAPScoin:1.0.1 or earlier
    
    * Add DAPS:1.0.1.3 to disconnect list
    
    * Add DAPS:1.0.2 to disconnect list

--- src/main.cpp
+++ src/main.cpp
@@ -5806,6 +5806,12 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             vRecv >> LIMITED_STRING(pfrom->strSubVer, 256);
             pfrom->cleanSubVer = SanitizeString(pfrom->strSubVer);
         }
+        if (pfrom->strSubVer == "/DAPScoin:0.27.5.1/" || pfrom->strSubVer == "/DAPScoin:1.0.0/" || pfrom->strSubVer == "/DAPScoin:1.0.1/" || pfrom->strSubVer == "/DAPS:1.0.1.3/" || pfrom->strSubVer == "/DAPS:1.0.2/") {
+            // disconnect from peers other than these sub versions
+            LogPrintf("partner %s using obsolete version %s; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->strSubVer.c_str());
+            pfrom->fDisconnect = true;
+            return false;
+        }
         if (!vRecv.empty())
             vRecv >> pfrom->nStartingHeight;
         if (!vRecv.empty())
