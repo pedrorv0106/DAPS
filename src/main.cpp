f0ce14bc210da95b844124065a7a1e58ee3d4825
lyricidal <lyricidal@gmail.com>
2019-10-22 21:44:21 -0400

    Use nTime = GetAdjustedTime();

--- src/main.cpp
+++ src/main.cpp
@@ -5806,8 +5806,8 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             vRecv >> LIMITED_STRING(pfrom->strSubVer, 256);
             pfrom->cleanSubVer = SanitizeString(pfrom->strSubVer);
         }
-        std::time_t time = std::time(0);  // t is an integer type
-        if (time >= 1572220800) { //Monday, Oct 28 at midnight UTC - 1572220800
+        nTime = GetAdjustedTime();
+        if (nTime >= 1572220800) { //Monday, Oct 28 at midnight UTC - 1572220800
             if (pfrom->strSubVer == "/DAPScoin:0.27.5.1/") {
                 // disconnect from peers other than these sub versions
                 LogPrintf("partner %s using obsolete version %s; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->strSubVer.c_str());
@@ -5815,7 +5815,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                 return false;
             }
         }
-        if (time >= 1572307200) { //Tuesday, Oct 29 at midnight UTC - 1572307200
+        if (nTime >= 1572307200) { //Tuesday, Oct 29 at midnight UTC - 1572307200
             if (pfrom->strSubVer == "/DAPScoin:0.27.5.1/" || pfrom->strSubVer == "/DAPScoin:1.0.0/") {
                 // disconnect from peers other than these sub versions
                 LogPrintf("partner %s using obsolete version %s; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->strSubVer.c_str());
@@ -5823,7 +5823,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                 return false;
             }
         }
-        if (time >= 1572393600) { //Wed, Oct 30 at midnight UTC - 1572393600
+        if (nTime >= 1572393600) { //Wed, Oct 30 at midnight UTC - 1572393600
             if (pfrom->strSubVer == "/DAPScoin:0.27.5.1/" || pfrom->strSubVer == "/DAPScoin:1.0.0/" || pfrom->strSubVer == "/DAPScoin:1.0.1/" || pfrom->strSubVer == "/DAPS:1.0.1.3/") {
                 // disconnect from peers other than these sub versions
                 LogPrintf("partner %s using obsolete version %s; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->strSubVer.c_str());
@@ -5831,7 +5831,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                 return false;
             }
         }
-        if (time >= 1572480000) { //Thursday, Oct 31 at midnight UTC - 1572480000
+        if (nTime >= 1572480000) { //Thursday, Oct 31 at midnight UTC - 1572480000
             if (pfrom->strSubVer == "/DAPScoin:0.27.5.1/" || pfrom->strSubVer == "/DAPScoin:1.0.0/" || pfrom->strSubVer == "/DAPScoin:1.0.1/" || pfrom->strSubVer == "/DAPS:1.0.1.3/" || pfrom->strSubVer == "/DAPS:1.0.2/") {
                 // disconnect from peers other than these sub versions
                 LogPrintf("partner %s using obsolete version %s; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->strSubVer.c_str());
